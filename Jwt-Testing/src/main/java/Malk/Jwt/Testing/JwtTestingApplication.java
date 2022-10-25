62e7fcf6518df5dbbd1f32e51902bb910e22293c
Malk <47364092+madmalk@users.noreply.github.com>
2022-10-25 15:35:58 -0300

    Add files via upload

---
+++ Jwt-Testing/src/main/java/Malk/Jwt/Testing/JwtTestingApplication.java
@@ -0,0 +1,13 @@
+package Malk.Jwt.Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTestingApplication.class, args);
+	}
+
+}
---
+++ Jwt-Testing/src/test/java/Malk/Jwt/Testing/JwtTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package Malk.Jwt.Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
